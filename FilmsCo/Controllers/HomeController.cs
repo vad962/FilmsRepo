64889743bf4288ab7e3050e86e8a756ed3f7cfda
eprey17 <eprey@mail.ru>
2020-01-16 20:01:07 +0500

    Список кинофильмов

--- FilmsCo/Controllers/HomeController.cs
+++ FilmsCo/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using FilmsCo.Models;
+using PagedList;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace FilmsCo.Controllers
 {
     public class HomeController : Controller
     {
-        
+        List<Film> Films;
 
-        public ActionResult Index()
+        public HomeController()
         {
-            return View();
+            Films = new List<Film>();
+            Films.Add(new Film { Name = "Samsung Galaxi" });
+            Films.Add(new Film { Name = "Samsung Galaxi II" });
+            Films.Add(new Film { Name = "Samsung Galaxi II" });
+            Films.Add(new Film { Name = "Samsung ACE" });
+            Films.Add(new Film { Name = "Samsung ACE II" });
+            Films.Add(new Film { Name = "HTC One S" });
+            Films.Add(new Film { Name = "HTC One X" });
+            Films.Add(new Film { Name = "Nokia N9" });
+        }
+
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(Films.ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult About()
